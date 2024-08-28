a03aef9e3accce76c9d49b44289a2e8bf1f1871b
Gabriel Gomes <gabrielgomesdeveloper1999@gmail.com>
2024-08-28 16:26:13 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+
+
+
+
+
+
+function App() {
+  return (
+    <>
+      <nav>
+        ReactFacts
+        <p>React Course - Project 1</p>
+      </nav>
+
+      <main>
+        <h1>Fun facts about React</h1>
+
+        <ul>
+          <li>Was first released in 2013 </li>
+          <li>Was originally created by Jordan Walke</li>
+          <li> Has well over 100K stars on GitHub</li>
+          <li>Is maintained by Facebook Powers</li>
+          <li>thousands of enterprise apps, including mobile apps</li>
+        </ul>
+      </main>
+    </>
+  );
+}
+
+export default App;
